6876ebdb661bec0a7041b0d9dc0af161e277ca88
LinkiuMatrix <linkiumatrix@soymrs.com>
2025-07-23 08:48:48 -0500

    Ajustes de build, tailwind y orden de imports

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,10 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-<<<<<<< HEAD
-import tailwindcss from '@tailwindcss/vite';
-=======
 
->>>>>>> eb4d7bce (Agrego archivos del proyecto dentro del repo)
 
 export default defineConfig({
     plugins: [
         laravel({
-<<<<<<< HEAD
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        tailwindcss(),
-    ],
-});
-=======
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
@@ -25,4 +13,3 @@ export default defineConfig({
         }),
     ],
 });
->>>>>>> eb4d7bce (Agrego archivos del proyecto dentro del repo)
